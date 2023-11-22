a8752abf6420ccbd9743632fbaab8fb177b4887e
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-11-22 11:45:10 +0100

    creazione

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PokedexHeader from './components/single-components/header'
+
+function App() {
+
+  return (
+    <>
+    <PokedexHeader/>
+      AAAAAAAAA
+    </>
+  )
+}
+
+export default App
