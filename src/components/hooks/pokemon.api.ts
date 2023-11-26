a4f1e211dbb2e19751de9b68d97e1a3e3cf18ebf
Aresso93 <ares.fiumicelli@gmail.com>
2023-11-26 01:34:34 +0100

    modifiche varie

--- src/components/hooks/pokemon.api.ts
+++ src/components/hooks/pokemon.api.ts
@@ -4,68 +4,62 @@ import { useAxios } from "../../services/axios-api";
 
 export function usePokemonApi() {
   const axiosService = useAxios();
-  let [pokemonDetail, setPokemonDetail] = useState([]);
-  let [genericData, setGenericData] = useState(0);
-  let [moveData, setMoveData] = useState([])
-  let [nextPage, setNextPage] = useState([])
-  let [previousPage, setPreviousPage] = useState([])
+  const [pokemonDetail, setPokemonDetail] = useState([]);
+  const [genericData, setGenericData] = useState(0);
+  const [moveData, setMoveData] = useState([]);
+  const [nextPage, setNextPage] = useState([]);
+  const [previousPage, setPreviousPage] = useState([]);
 
-  let offset = 0
-  let currentPage = 1
-  function listPokemonByID(a: Pokemon, b: Pokemon) {
-    return a.id - b.id;
-  }
-  
+  let offset = 0;
+  let currentPage = 1;
+
   async function getPokemonData() {
     const firstResponse = await axiosService("pokemon/");
-    let firstArray = firstResponse.data.results;
-    let detailPokemonArray: Pokemon[] = [];
-    firstArray.map(
-      async (singlePokemon: Pokemon) =>
-      await axiosService(singlePokemon.url).then((secondResponse) => {
-        detailPokemonArray.push(secondResponse.data);
-        const orderedArray = detailPokemonArray.sort(listPokemonByID);
-        setPokemonDetail(orderedArray);
-        console.log('UGA BUGA', detailPokemonArray);
-        return orderedArray;
-        
+    const firstArray = firstResponse.data.results;
+
+    const detailPokemonArray = await Promise.all(
+      firstArray.map(async (singlePokemon: Pokemon) => {
+        const secondResponse = await axiosService(singlePokemon.url);
+        return secondResponse.data;
       })
-      );
-    }
-    
-    async function getMoveData(){
-      const movesResp = await axiosService("move/");
-      setMoveData(movesResp.data)
-    }
-    
-    async function getData(){
-      const response = await axiosService("pokemon/");
-      setGenericData(response.data.count)
-    }
-    
-    async function getOnlyData(){
-     const resp = await axiosService("pokemon/?offset="+offset+"&limit=20");
-     return resp.data.results
-    }
+    );
 
-    async function getNextPage(){
-    currentPage++
-    offset = offset + 20
-    let nextResp = await getOnlyData()
-    console.log(nextResp);
-    
-    }
-
-  async function getPreviousPage(){
-    currentPage--
-    if (offset > 0){
-      offset = offset - 20
-      let prevResp = await getOnlyData()
-      console.log(prevResp);
-    }
-    
+    const orderedArray = detailPokemonArray;
+    setPokemonDetail(orderedArray);
+    console.log("UGA BUGA", detailPokemonArray);
+    return orderedArray;
   }
 
+  async function getMoveData() {
+    const movesResp = await axiosService("move/");
+    setMoveData(movesResp.data);
+  }
+
+  async function getData() {
+    const response = await axiosService("pokemon/");
+    setGenericData(response.data.count);
+  }
+
+  async function getOnlyData() {
+    const resp = await axiosService("pokemon/?offset=" + offset + "&limit=20");
+    return resp.data.results;
+  }
+
+  async function getNextPage() {
+    currentPage++;
+    offset = offset + 20;
+    const nextResp = await getOnlyData();
+    console.log(nextResp);
+  }
+
+  async function getPreviousPage() {
+    currentPage--;
+    if (offset > 0) {
+      offset = offset - 20;
+      const prevResp = await getOnlyData();
+      console.log(prevResp);
+    }
+  }
 
   return {
     actions: {
@@ -73,13 +67,13 @@ export function usePokemonApi() {
       getMoveData,
       getData,
       getNextPage,
-      getPreviousPage
+      getPreviousPage,
     },
     states: {
-        pokemonDetail, 
-        genericData,
-        moveData,
-        currentPage
+      pokemonDetail,
+      genericData,
+      moveData,
+      currentPage,
     },
   };
 }
--- src/components/pages/pokemon-card.tsx
+++ src/components/pages/pokemon-card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
